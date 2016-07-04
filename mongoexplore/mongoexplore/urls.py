947961415180ddeea1a31971ed2156332bd59ab3
Gayathri-Palanimuthu <gayathri.palanimuthu@above-inc.com>
2016-07-04 12:28:39 +0530

    Commit URL string capture and gitignore

--- mongoexplore/mongoexplore/urls.py
+++ mongoexplore/mongoexplore/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'users.views.create', name='create'),
+    url(r'^create/(?P<anystring>[-\w]+)/$', 'users.views.create', name='create'),
     # url(r'^mongoexplore/', include('mongoexplore.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- mongoexplore/users/views.py
+++ mongoexplore/users/views.py
@@ -3,18 +3,29 @@ from models import Employee, User, Student
 from django.http import HttpResponse
 # Create your views here.
 
-def create(request):
-    user = User.objects.create(
-        email="pedro.kong@company.com",
-        first_name="Pedro",
-        last_name="Kong"
+def create(request, anystring ):
+    if anystring == 'employee':
+        user = User.objects.create(
+            email="pedro.kong@company.com",
+            first_name="Pedro",
+            last_name="Kong"
         
-    )
-    employee = Employee(employee_id="123", designation="Maths Professor")
-    user.employees.append(employee)
-    user.save()
-    return HttpResponse("Employee created")
-
+        )
+        employee = Employee(employee_id="123", designation="Maths Professor")
+        user.employees.append(employee)
+        user.save()
+        return HttpResponse("Employee created")
+    elif anystring == 'student':
+        user = User.objects.create(
+            email="gayathri.mithu@school.com",
+            first_name="Gayathri",
+            last_name="Muthu"
+        
+        )
+        student = Student(student_id="123", grade="plus2")
+        user.students.append(student)
+        user.save()
+        return HttpResponse("Student created")
 '''
 def employee(request):
     employee = Employee.objects.create(
